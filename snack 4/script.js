e46a6fe773520c2014d22d0de08d5f07d66b4775
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-13 12:23:24 +0200

    corretto

--- snack 4/script.js
+++ snack 4/script.js
@@ -1,6 +1,6 @@
 const nameGatsby= ["matteo","andrea","giovanni","riccardo","davide","simone"]
 let nomeUtente=prompt("scrivi il tuo nome per vedere se puoi partecipare alla festa")
-if (nomeUtente=nameGatsby) {
+if (nameGatsby.includes(nomeUtente)) {
     console.log("sei il benvenuto")
 }else{
     console.log("non sei il benvenuto")
