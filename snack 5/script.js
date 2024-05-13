9668629289ad22fd6a398929beced8779c794947
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-13 12:32:37 +0200

    snack 5

---
+++ snack 5/script.js
@@ -0,0 +1,10 @@
+const numeri=[]
+for (let i=0; i < 6; i++){
+    let numberUser=parseInt(prompt("inserisci un numero"),10)
+    if (numberUser %2==1){
+        numeri.push(numberUser)
+
+    }
+   
+}
+console.log(numeri)
