f2c89cb40441b71a23749db5d9b461ef50be0ee5
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-13 11:47:01 +0200

    first commit

---
+++ snack 1/script.js
@@ -0,0 +1,11 @@
+// L'utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+let numeroUno=prompt("scrivi un numero")
+let numeroDue=prompt("scrivi un numero")
+
+if ( numeroUno > numeroDue){
+    console.log(numeroUno)
+}else if(numeroDue > numeroUno){
+    console.log(numeroDue)
+}
