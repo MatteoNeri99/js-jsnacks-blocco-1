f74c449c98c4713d5c3339517e48f55cb6897a53
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-14 10:59:14 +0200

    es array 1

--- snack 5/script.js
+++ snack 5/script.js
@@ -3,8 +3,6 @@ for (let i=0; i < 6; i++){
     let numberUser=parseInt(prompt("inserisci un numero"),10)
     if (numberUser %2==1){
         numeri.push(numberUser)
-
     }
-   
 }
 console.log(numeri)
---
+++ snack array/script.js
@@ -0,0 +1,11 @@
+const numeriInteri=[1,2,3,4,5,6]
+
+let somma=0;
+
+for (i=0; i<numeriInteri.lenght; i++  ){
+    if (index %2===1){
+        somma=somma + numeriInteri[index]
+    }
+
+}
+console.log(somma)
