46a8405db519916355b11fec525bf3cd7cf080ac
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-14 12:17:23 +0200

    es snack 8

--- snack 8/scrip.js
+++ snack 8/scrip.js
@@ -1,19 +1,30 @@
 const divUno=document.createElement("div");
 
 const divDue=document.createElement("div");
+let mySection=document.querySelector(".containerEl");
 
-divUno.classList.add("red")
+mySection.appendChild(divUno);
 
-divDue.classList.add("green")
+mySection.appendChild(divDue);
+
+divUno.classList.add("red");
+
+divDue.classList.add("green");
 
 
-const numeriCasuali=[1,2,3,4,6,8,9,0]
+const numeriCasuali=[1,2,3,4,6,8,9,0];
 
-if(numeriCasuali[index] %2===1){
-    divUno.append(numeriCasuali)
-}else(numeriCasuali[index] %2===0)
-    divDue.append(numeriCasuali)
+for (index=0; index<numeriCasuali; index++){
+
+    if(numeriCasuali[index] %2===1){
+
+        divUno.append(numeriCasuali)
+
+    }else(numeriCasuali[index] %2===0)
+
+        divDue.append(numeriCasuali)
     
+}
 
 
 
