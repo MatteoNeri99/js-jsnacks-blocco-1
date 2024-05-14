95fbf9b501a69e438ab73c6acfcc0362175114a0
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-14 11:59:22 +0200

    es snack 8

---
+++ snack 8/scrip.js
@@ -0,0 +1,20 @@
+const divUno=document.createElement("div");
+
+const divDue=document.createElement("div");
+
+divUno.classList.add("red")
+
+divDue.classList.add("green")
+
+
+const numeriCasuali=[1,2,3,4,6,8,9,0]
+
+if(numeriCasuali[index] %2===1){
+    divUno.append(numeriCasuali)
+}else(numeriCasuali[index] %2===0)
+    divDue.append(numeriCasuali)
+    
+
+
+
+
